8c6f1b15c012281f12205c78c48e13098f67b36b
thoakSoe <thantoaksoe.39@gmail.com>
2025-10-17 13:24:30 +0630

    something

---
+++ ForMMS/ForMMS/src/main/java/com/ojt/ForMMS/ForMmsApplication.java
@@ -0,0 +1,13 @@
+package com.ojt.ForMMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForMmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForMmsApplication.class, args);
+	}
+
+}
---
+++ ForMMS/ForMMS/src/test/java/com/ojt/ForMMS/ForMmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojt.ForMMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForMmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
